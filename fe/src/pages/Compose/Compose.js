4fd1cd75d676d391ac495428f3d81cb5469b738d
ptlharshil <patelharshil3695@gmail.com>
2022-12-10 20:26:35 -0500

    connect be with fe and db

--- fe/src/pages/Compose/Compose.js
+++ fe/src/pages/Compose/Compose.js
@@ -99,12 +99,12 @@ const Compose = () => {
         "Compose.js: fetching from " +
           `${process.env.REACT_APP_API_SERVICE_URL}/tweet`
       );
-      // const response = await fetch("http://localhost:5000/tweet", config);
+       const response = await fetch("http://localhost:5000/tweet", config);
       //const response = await fetch(`${process.env.REACT_APP_BE_NETWORK}:${process.env.REACT_APP_BE_PORT}/tweet`, config);
-      const response = await fetch(
-        `${process.env.REACT_APP_API_SERVICE_URL}/tweet`,
-        config
-      );
+      // const response = await fetch(
+      //   `${process.env.REACT_APP_API_SERVICE_URL}/tweet`,
+      //   config
+      // );
       // const response = await fetch(`tweet`, config);
       //const json = await response.json()
       if (response.ok) {
